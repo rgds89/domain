81cd91e0411edeac68bd18d2b8d3a73f9abc0208
Roger Silva <rogersilvati@gmail.com>
2021-09-06 16:52:39 -0300

    Start of the Domain Model Project

---
+++ src/main/java/com/devsuperior/domain/DomainApplication.java
@@ -0,0 +1,21 @@
+package com.devsuperior.domain;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
